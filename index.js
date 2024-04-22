0f2da26f1d5237cb95f34d6db00c17d3d72277cf
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-04-22 17:16:22 +0600

    netlify link added

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(cors({
     origin: [
         // 'http://localhost:5173',
         'https://car-doctor-client-99145.web.app',
-        'https://car-doctor-client-99145.firebaseapp.com'
+        'https://car-doctor-client-99145.firebaseapp.com',
+        'https://car-doctor-client-nion.netlify.app'
     ],
     credentials: true
 }));
